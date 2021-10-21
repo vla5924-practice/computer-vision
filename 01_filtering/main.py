82ffb970474544efc5d27164d1a9f563857947e0
vla5924 <vla5924@gmail.com>
2021-10-21 20:03:33 +0300

    connectedComponentsWithStats

--- 01_filtering/main.py
+++ 01_filtering/main.py
@@ -11,7 +11,13 @@ cropped = image[faces[0][1]:faces[0][1] + faces[0]
                 [3], faces[0][0]:faces[0][0] + faces[0][2]]
 
 edged = cv2.Canny(cv2.cvtColor(cropped, cv2.COLOR_BGR2GRAY), 100, 200)
-# TODO: Remove edges <10px???
+width, height = edged.shape
+
+_, labels, stats, _ = cv2.connectedComponentsWithStats(edged, 4)
+for x in range(width):
+    for y in range(height):
+        if stats[labels[x, y], cv2.CC_STAT_WIDTH] <= 10 and stats[labels[x, y], cv2.CC_STAT_HEIGHT] <= 10:
+            edged[x, y] = 0
 
 kernel = numpy.array([
     [0, 0, 1, 0, 0],
@@ -20,7 +26,6 @@ kernel = numpy.array([
     [0, 1, 1, 1, 0],
     [0, 0, 1, 0, 0],
 ], dtype=numpy.uint8)
-
 dilated = cv2.dilate(edged, kernel)
 
 blurred = cv2.GaussianBlur(dilated, (5, 5), 0, 0)
